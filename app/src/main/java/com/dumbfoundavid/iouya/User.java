9330cc8042aa97aaadb5a33d03c0fcfe01822e88
dumbfoundavid <davperez94@gmail.com>
2016-09-20 13:24:41 -0700

    added more variables to User.java

--- app/src/main/java/com/dumbfoundavid/iouya/User.java
+++ app/src/main/java/com/dumbfoundavid/iouya/User.java
@@ -1,12 +1,21 @@
 package com.dumbfoundavid.iouya;
 
+import java.util.ArrayList;
+
 /**
  * Created by DNP on 9/20/16.
  */
 
 public class User {
 
-    private String firstName;
+    private String id;
+    private String name;
+    private String userName;
+    private String password;
+    private String photo;
+    private Tree tree;
+    private ArrayList friends;
+
 
 
 }
