6d7a30c488e20fbab7cac032295101a32924dc91
dumbfoundavid <davperez94@gmail.com>
2016-09-20 13:33:25 -0700

    added getters and setters

--- app/src/main/java/com/dumbfoundavid/iouya/User.java
+++ app/src/main/java/com/dumbfoundavid/iouya/User.java
@@ -16,6 +16,64 @@ public class User {
     private Tree tree;
     private ArrayList<User> friends;
 
+    public User()
+    {
 
+    }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Tree getTree() {
+        return tree;
+    }
+
+    public void setTree(Tree tree) {
+        this.tree = tree;
+    }
+
+    public ArrayList<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(ArrayList<User> friends) {
+        this.friends = friends;
+    }
 }
